a992ee73443bad4f756bed768abf075f41d134ec
Dejan Lavbič <Dejan@lavbic.net>
2021-04-26 05:59:23 +0000

    Initial commit

---
+++ knjiznice/js/koda.js
@@ -0,0 +1,22 @@
+var baza = 'TO SPREMENITE NA VAŠE UPORABNIŠKO IME';
+var baseUrl = 'https://teaching.lavbic.net/api/OIS/baza/' + baza + '/podatki/';
+
+
+/**
+ * Generator podatkov za novega pacienta, ki bo uporabljal aplikacijo. Pri
+ * generiranju podatkov je potrebno najprej kreirati novega pacienta z
+ * določenimi osebnimi podatki (ime, priimek in datum rojstva) ter za njega
+ * shraniti nekaj podatkov o vitalnih znakih.
+ * @param stPacienta zaporedna številka pacienta (1, 2 ali 3)
+ * @return ehrId generiranega pacienta
+ */
+function generirajPodatke(stPacienta) {
+  ehrID = "";
+
+  // TODO: Potrebno implementirati
+
+  return ehrID;
+}
+
+
+// TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
